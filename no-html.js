08d3b8b9a1022608c877ca67d9055179646d9d38
Derrick Schwabe <admin@nerdbusiness.com>
2020-09-16 14:34:25 -0600

    Establish initial API with goal of offering a set of abstract functions that can quickly get you serving an HTML page and watch'ed JS bundle ie- from one file, or 2 if you count a client.js target for bundle)

---
+++ no-html.js
@@ -0,0 +1,104 @@
+const no = {}
+
+no.html = (css, body, script) => {
+	return `
+<html>
+<head>
+	<style>
+		${css ? css : '' }
+	</style>
+</head>
+<body>
+	${body ? body : ''}
+	${script ? `<script src="${script}"></script>` : '' }
+</body>
+</html>
+`
+}
+
+const fs = require('fs')
+no.makeIndex = (path, html) => {
+	if(!path) path = process.cwd() + '/index.html'
+	if(!html) html = no.html()
+		debugger 
+	fs.writeFileSync(path, html )
+}
+
+
+//no.css = () => `${masscss}`
+
+const express = require('express')
+no.server = port => {
+	if(!port) port = 8000
+	no.expressApp = express()
+	no.expressApp.listen(port, () => console.log('http://localhost:' + port))
+	return no.expressApp
+}
+
+const _s = require('underscore.string')
+no.static = directory => {
+	if(!directory) directory =  _s.strLeftBack( require.main.filename, '/' )
+	no.expressApp.use(express.static(directory))
+}
+
+no.index = html => {
+	if(!no.expressApp) no.server()
+	if(!html) html = no.html()
+	//should autodetect if a client.bundle.js file is on fs
+	no.expressApp.get('/', (req, res) => {
+		res.send( html )
+	})
+}
+
+
+
+//bundle...
+const browserify = require('browserify')
+const stringify = require('stringify')
+const watchify = require('watchify')
+
+//no.bundle = bundleName => bundle(bundleName)
+const path = require('path')
+
+no.watch = (clientJsName, bundleName) => {
+	if(!clientJsName) clientJsName = 'client.js'
+	if(!bundleName) bundleName = 'client.bundle.js'
+
+	let directory = _s.strLeftBack( require.main.filename, '/' )
+	let b = browserify({
+		cache: {},
+		packageCache: {},
+		debug : true,
+		basedir : directory,
+		commondir : false
+	})
+
+	const bundle = () => {
+		b.bundle( (err, buff) => {
+			if(err) return console.warn(err)
+			console.log('writing new bundle...')
+			fs.writeFile(`${directory}/${bundleName}`,buff, (err) => {
+				if(err) return console.warn(err)
+				console.log(`wrote to ${bundleName}`)
+			})
+		})
+		.on('error', console.error)
+	}
+
+	b.plugin(watchify)
+	b.transform(stringify({
+		extensions: [ '.html' , '.css', '.svg'],
+	}), {
+		global: true
+	})
+	b.add(`${directory}/${clientJsName}`)
+	b.transform("babelify", {
+		presets: ["@babel/preset-env"],
+		sourceType : 'unambiguous',
+		global: true
+	})
+	b.on('update', bundle)
+	bundle()
+}
+
+module.exports = no
